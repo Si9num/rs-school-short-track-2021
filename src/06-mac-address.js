6d2b70d11d788824c047a0d5e80879982d8c441b
Si9num <78493168+Si9num@users.noreply.github.com>
2021-04-06 17:23:20 +0300

    feat: add a solution

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,18 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  let arr = [];
+  arr = n.split('');
+  let arr2 = [];
+  let res = Boolean;
+  arr2 = arr.filter((x) => x === '' || x === '0' || x === '1' || x === '2' || x === '3' || x === '4' || x === '5' || x === '6' || x === '7' || x === '8' || x === '9' || x === '-' || x === 'A' || x === 'B' || x === 'C' || x === 'D' || x === 'E' || x === 'F');
+  if (arr.length === arr2.length) {
+    res = true;
+  } else {
+    res = false;
+  }
+  return res;
 }
 
 module.exports = isMAC48Address;
