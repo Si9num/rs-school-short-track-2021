340174e75a7dd45301a2642a0fa5f3888bd3d4a1
Si9num <78493168+Si9num@users.noreply.github.com>
2021-04-06 21:14:39 +0300

    feat: add  a solution

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,18 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1] && arr[i] !== arr[i + 2] && arr[i] !== arr[i + 3]) {
+      arr.splice(i, 1, 2);
+    } else if (arr[i] === arr[i + 1] && arr[i] === arr[i + 2] && arr[i] !== arr[i + 3]) {
+      arr.splice(i, 2, 3);
+    } else if (arr[i] === arr[i + 1] && arr[i] === arr[i + 2]) {
+      arr.splice(i, 3, 4);
+    }
+  }
+  return arr.join('');
 }
 
 module.exports = encodeLine;
